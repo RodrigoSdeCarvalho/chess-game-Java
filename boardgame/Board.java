c5b30d9c0c62ff55dd02bd9c00f1f71edd120a74
RodrigoSdeCarvalho <rodrigosc2401@gmail.com>
2022-06-27 01:38:29 -0300

    Defensive Programming on Board class

--- boardgame/Board.java
+++ boardgame/Board.java
@@ -8,36 +8,56 @@ public class Board {
     public Board(int rows, int columns) {
         this.rows = rows;
         this.columns = columns;
+        areBoardDimensionsValid();
         piecesMatrix = new Piece[rows][columns];
+    }
+
+    private void areBoardDimensionsValid() {
+        if (this.rows < 1 || this.columns <1) {
+            throw new BoardException("Error in creating board: there must be at least 1 row and 1 column");
+        }
     }
 
     public int getRows() {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getColumns() {
         return columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
-
     public Piece piece(int row, int column) {
+        isPiecePositionValid(row, column);
         return piecesMatrix[row][column];
     }
 
     public Piece piece(Position position) {
+        isPiecePositionValid(position.getRow(), position.getColumn());
         return piecesMatrix[position.getRow()][position.getColumn()];
     }
 
+    private void isPiecePositionValid(int row, int column) {
+        if (!positionOnBoard(row, column)) {
+            throw new BoardException("Position not on the board");
+        }
+    }
+
+    private void isPiecePositionValid(Position position) {
+        if (!positionOnBoard(position.getRow(), position.getColumn())) {
+            throw new BoardException("Position not on the board");
+        }
+    }
+
     public void placePieceOnBoard(Piece piece, Position piecePosition) {
+        isPositionNotOccupied(piecePosition);
         piecesMatrix[piecePosition.getRow()][piecePosition.getColumn()] = piece;
         piece.position = piecePosition;
+    }
+
+    private void isPositionNotOccupied(Position position) {
+        if (isThereAPieceOnThisPosition(position)) {
+            throw new BoardException("There's already a piece on position " + position);
+        }
     }
 
     public boolean positionOnBoard(int row, int column) {
@@ -55,6 +75,7 @@ public class Board {
     }
 
     public boolean isThereAPieceOnThisPosition(Position position) {
+        isPiecePositionValid(position);
         return piece(position) != null;
     }
 }
